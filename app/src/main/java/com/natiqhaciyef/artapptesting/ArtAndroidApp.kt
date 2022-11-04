63eb42db9baefc7ada9f9818d12601a966a69c20
Natiq <natiq00h2272@gmail.com>
2022-11-04 22:21:39 +0400

    Dependency Injection instance 1.0.5

---
+++ app/src/main/java/com/natiqhaciyef/artapptesting/ArtAndroidApp.kt
@@ -0,0 +1,7 @@
+package com.natiqhaciyef.artapptesting
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ArtAndroidApp : Application()
---
+++ app/src/main/java/com/natiqhaciyef/artapptesting/di/AppModule.kt
@@ -0,0 +1,39 @@
+package com.natiqhaciyef.artapptesting.di
+
+import android.content.Context
+import androidx.room.Room
+import com.natiqhaciyef.artapptesting.retrofit.api.PixabayAPI
+import com.natiqhaciyef.artapptesting.retrofit.util.RetrofitDetails.BASE_URL
+import com.natiqhaciyef.artapptesting.roomdb.ArtDatabase
+import com.natiqhaciyef.artapptesting.roomdb.ArtModel
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ActivityComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun injectRoomDB(@ApplicationContext context: Context) =
+        Room.databaseBuilder(context, ArtDatabase::class.java, "ArtDatabase").build()
+
+    @Singleton
+    @Provides
+    fun injectDao(database: ArtDatabase) = database.artDao()
+
+    @Singleton
+    @Provides
+    fun injectRetrofit(): PixabayAPI =
+        Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(PixabayAPI::class.java)
+}

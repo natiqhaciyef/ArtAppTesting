b3314a0ab1a376305b09e37ed45c60d77567c2e0
Natiq <natiq00h2272@gmail.com>
2022-11-10 18:33:54 +0400

    Last Bug fixing

---
+++ app/src/main/java/com/natiqhaciyef/artapptesting/retrofit/util/RetrofitDetails.kt
@@ -0,0 +1,6 @@
+package com.natiqhaciyef.artapptesting.retrofit.util
+
+object RetrofitDetails {
+    const val API_KEY = "31094586-e0a6473fddb0e1873426a42d6"
+    const val BASE_URL = "https://pixabay.com"
+}
